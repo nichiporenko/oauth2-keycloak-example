3625a311f9d7109dc26eb4eb90814e8a7765a16b
Dmitry Nichiporenko <excelsiorious@gmail.com>
2022-10-18 23:54:07 +0300

    Fix working setup via gateway

--- gateway/src/main/java/com/nichiporenko/gateway/config/SecurityConfig.java
+++ gateway/src/main/java/com/nichiporenko/gateway/config/SecurityConfig.java
@@ -6,12 +6,16 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+import org.springframework.security.web.server.authentication.logout.*;
 
 @Configuration
 @EnableWebSecurity
 class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private static final String FRONTEND_URL = "http://localhost:8072";
 
     @Bean
     protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
@@ -27,27 +31,7 @@ class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .oauth2Login()
                 .defaultSuccessUrl("/", false)
-                .and().cors()
-                .and()
-                .logout()
-                //.addLogoutHandler(keycloakLogoutHandler)
-                .logoutSuccessUrl("/");
-                //.antMatchers("", "/oauth2/**")
-
-//                .anyRequest().authenticated()
-
-//                .antMatchers("/", "/login**","/callback/", "/webjars/**", "/error**")
-//                .anyRequest()
-//                .permitAll();
-//                .authenticated();
-//                .and()
-//                        .formLogin();
-                //.exceptionHandling()
-                //.authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("http://localhost:8079/auth/admin/Test_realm/console/"));
-//        http.oauth2Login()
-//                .defaultSuccessUrl("/", false)
-//                //.successHandler(new SimpleUrlAuthenticationSuccessHandler("http://localhost:8072/ui/"))
-////                .loginPage("/auth/admin/Test_realm/console/")
+                .and().cors();
 //                .and()
 //                .logout()
 //                .addLogoutHandler(keycloakLogoutHandler)
