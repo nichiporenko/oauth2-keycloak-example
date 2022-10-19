be98b800c70cf43da8fa9ee85a9eabb08dbaaa70
Dmitry Nichiporenko <excelsiorious@gmail.com>
2022-10-19 14:34:38 +0300

    Add logout endpoint, refactoring

--- gateway/src/main/java/com/nichiporenko/gateway/config/SecurityConfig.java
+++ gateway/src/main/java/com/nichiporenko/gateway/config/SecurityConfig.java
@@ -5,37 +5,44 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.authentication.logout.*;
 
 @Configuration
 @EnableWebSecurity
-class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    private static final String FRONTEND_URL = "http://localhost:8072";
+class SecurityConfig {
 
     @Bean
     protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
         return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/oauth2/**", "/login/**").permitAll()
-                //.antMatchers( "/*", "/reviews*")
-                .anyRequest().authenticated()
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http, ServerLogoutSuccessHandler handler) {
+        http.authorizeExchange()
+                .pathMatchers("/oauth2/**", "/login/**").permitAll()
+                .and().authorizeExchange().anyExchange().authenticated()
                 .and()
                 .oauth2Login()
-                .defaultSuccessUrl("/", false)
-                .and().cors();
-//                .and()
-//                .logout()
-//                .addLogoutHandler(keycloakLogoutHandler)
-//                .logoutSuccessUrl("/");
+                .and()
+                .logout().logoutSuccessHandler(handler);
+
+        return http.build();
+    }
+
+    @Bean
+    public ServerLogoutSuccessHandler keycloakLogoutSuccessHandler(ReactiveClientRegistrationRepository repository) {
+        OidcClientInitiatedServerLogoutSuccessHandler oidcLogoutSuccessHandler =
+                new OidcClientInitiatedServerLogoutSuccessHandler(repository);
+
+        oidcLogoutSuccessHandler.setPostLogoutRedirectUri("{baseUrl}/ui/");
+        return oidcLogoutSuccessHandler;
     }
 
 }
--- ui/src/main/java/com/nichiporenko/controller/MainController.java
+++ ui/src/main/java/com/nichiporenko/controller/MainController.java
@@ -4,10 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @Controller
 public class MainController {
@@ -23,22 +19,4 @@ public class MainController {
 
         return "data";
     }
-
-//    @GetMapping("/logout")
-//    public String logout(HttpServletRequest request, HttpServletResponse response) {
-//        try {
-//
-//
-//            Cookie[] cookies = request.getCookies();
-//            for (Cookie cookie : cookies) {
-//                if ("".equals(cookie.getName())) {
-//
-//                }
-//            }
-//            response.sendRedirect("");
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
-//
-//    }
 }
